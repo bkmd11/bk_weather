286a141e3e183b7f440d2e3465fc0be19590658d
bkmd11 <bkendall11@gmail.com>
2020-10-17 11:25:48 -0400

    set different text to each tab

--- app/src/main/java/com/bkendall/bk_weather/ShowWeatherFragment.java
+++ app/src/main/java/com/bkendall/bk_weather/ShowWeatherFragment.java
@@ -12,6 +12,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class ShowWeatherFragment extends Fragment {
+
+    private static final String ARG_COUNT = "param1";
     private Integer count;
 
     public ShowWeatherFragment() {
@@ -20,6 +22,9 @@ public class ShowWeatherFragment extends Fragment {
 
     public static ShowWeatherFragment newInstance(Integer count) {
         ShowWeatherFragment fragment = new ShowWeatherFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COUNT, count);
+        fragment.setArguments(args);
 
         return fragment;
     }
@@ -28,22 +33,29 @@ public class ShowWeatherFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            count = 3;
+            count = getArguments().getInt(ARG_COUNT);
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.fragment_show_weather, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        String currentWeather = getString(R.string.current_weather);
+        String hrByHr = getString(R.string.hr_by_hr_forecast);
+        String threeDay = getString(R.string.future_forecast);
+
+        final String[] tabText = {currentWeather, hrByHr, threeDay};
+
         super.onViewCreated(view, savedInstanceState);
 
-        TextView textViewCounter = view.findViewById(R.id.spam);
-        textViewCounter.setText(R.string.current_weather);
+       // FetchWeather.getForecast("69", "69", R.);
+
+        TextView textViewCounter = view.findViewById(R.id.weather_view);
+        textViewCounter.setText(tabText[count]);
     }
 }
