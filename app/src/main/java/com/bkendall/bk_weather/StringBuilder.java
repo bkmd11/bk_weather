bf856e7827221099e677132d911a7594df8e540c
bkmd11 <bkendall11@gmail.com>
2020-10-27 18:14:39 -0400

    refactoring to make more pythonic

--- app/src/main/java/com/bkendall/bk_weather/StringBuilder.java
+++ app/src/main/java/com/bkendall/bk_weather/StringBuilder.java
@@ -18,20 +18,20 @@ import java.util.Locale;
 public class StringBuilder {
 
     @SuppressLint("DefaultLocale")
-    public static String setCurrentWeatherString(JSONObject currentWeather) throws JSONException {
+    public static String setCurrentWeatherString(JSONObject currentWeather, String weatherString) throws JSONException {
         // I build the string for the current weather tab
         String description = setConditions(currentWeather);
 
         int temp = setDoubleToInt(currentWeather.getDouble("temp"));
 
-        return String.format("Current Weather\n\t\t\t\t%s\n\t\t\t\t%d", description, temp);
+        return String.format(weatherString, description, temp);
     }
 
     @SuppressLint("DefaultLocale")
-    public static String setHourByHourString(JSONArray hrByHr) throws JSONException {
+    public static String setHourByHourString(JSONArray hrByHr, String weatherString) throws JSONException {
         // I loop through a JSONArray to build an hour by hour string
         String hrByHrForecastString = "";
-        String hrString = "%s\n\t\t\t\t%s\n\t\t\t\tTemp: %d\n\n";
+
         int unix_time;
         int i;
 
@@ -48,17 +48,17 @@ public class StringBuilder {
 
             String conditions = setConditions(hourlyForecast);
 
-            hrByHrForecastString = hrByHrForecastString.concat(String.format(hrString, hourString, conditions, temp_int));
+            hrByHrForecastString = hrByHrForecastString.concat(String.format(weatherString, hourString, conditions, temp_int));
         }
 
         return hrByHrForecastString;
     }
 
     @SuppressLint("DefaultLocale")
-    public static String setFutureForecastString(JSONArray futureForecast) throws JSONException {
+    public static String setFutureForecastString(JSONArray futureForecast, String weatherString) throws JSONException {
         // I loop through a JSONArray to build a daily forecast string
         String futureForecastString = "";
-        String forecastString = "%s\n\t\t\t\t%s\n\t\t\t\tHigh: %d\n\t\t\t\tLow: %d\n\n";
+
         int unix_time;
         int i;
 
@@ -76,7 +76,7 @@ public class StringBuilder {
 
             String conditions = setConditions(dailyForecast);
 
-            futureForecastString = futureForecastString.concat(String.format(forecastString, weekday, conditions, max_temp, min_temp));
+            futureForecastString = futureForecastString.concat(String.format(weatherString, weekday, conditions, max_temp, min_temp));
         }
 
         return futureForecastString;
