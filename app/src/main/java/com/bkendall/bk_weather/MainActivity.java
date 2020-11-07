cd96bc3206c90a915f33ecea2aff9433400c6f46
bkmd11 <bkendall11@gmail.com>
2020-11-07 12:25:52 -0500

    fixed first launch crash bug

--- app/src/main/java/com/bkendall/bk_weather/MainActivity.java
+++ app/src/main/java/com/bkendall/bk_weather/MainActivity.java
@@ -14,10 +14,11 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+
+import org.jetbrains.annotations.NotNull;
 
 // TODO: this thing can't really handle errors
 public class MainActivity extends AppCompatActivity {
@@ -28,26 +29,64 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ){
+             ActivityCompat.requestPermissions(this,
+                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+         else {
+             setCoordinates();
+             setForecastViews();
+         }
+    }
+
+    private WeatherFragmentAdapter createMyAdapter() throws InterruptedException {
+        return new WeatherFragmentAdapter(this, this, lat, lon);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NotNull String[] permissions,
+                                           @NotNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        setCoordinates();
+        setForecastViews();
+    }
+
+    private void setForecastViews(){
         // Tab titles
         String current_weather = getString(R.string.current);
         String hr_by_hr = getString(R.string.hr_by_hr);
         String future_forecast = getString(R.string.future_cast);
         final String[] tabTitles = {current_weather, hr_by_hr, future_forecast};
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager2 = findViewById(R.id.viewPager2);
 
         try {
+            viewPager2.setAdapter(createMyAdapter());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new TabLayoutMediator(tabLayout, viewPager2,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        tab.setText(tabTitles[position]);
+                    }
+                }).attach();
+
+    }
+
+    private void setCoordinates(){
+        try {
             LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }
-
             assert lm != null;
-            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER); // TODO: look to update to get current location
+            @SuppressLint("MissingPermission") Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER); // TODO: look to update to get current location
 
             assert location != null;
             lat = location.getLatitude();
@@ -57,30 +96,6 @@ public class MainActivity extends AppCompatActivity {
             lat = 0;
             lon = 0;
         }
-
-        if (savedInstanceState == null) {
-            tabLayout = findViewById(R.id.tabLayout);
-            viewPager2 = findViewById(R.id.viewPager2);
-
-            try {
-                viewPager2.setAdapter(createMyAdapter());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            new TabLayoutMediator(tabLayout, viewPager2,
-                    new TabLayoutMediator.TabConfigurationStrategy() {
-                        @Override
-                        public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
-                            tab.setText(tabTitles[position]);
-                        }
-                    }).attach();
-        }
-    }
-
-
-    private WeatherFragmentAdapter createMyAdapter() throws InterruptedException {
-        return new WeatherFragmentAdapter(this, this, lat, lon);
     }
 }
 
